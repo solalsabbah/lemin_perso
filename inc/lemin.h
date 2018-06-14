76e97b33267dc89c0fa8ff1febf6a3dcb6fec829
solalsabbah <solal.sabbah@gmail.com>
2018-06-14 15:34:25 +0200

    starting trees

--- inc/lemin.h
+++ inc/lemin.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.h                                          :+:      :+:    :+:   */
+/*   lemin.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ssabbah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/14 11:53:30 by ssabbah           #+#    #+#             */
-/*   Updated: 2018/06/12 13:23:36 by ssabbah          ###   ########.fr       */
+/*   Created: 2018/06/14 14:35:06 by ssabbah           #+#    #+#             */
+/*   Updated: 2018/06/14 15:34:13 by ssabbah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,10 @@
 
 typedef struct s_node
 {
-    unsigned int key;
-    struct s_node *left;
-    struct s_node *right;
+    unsigned int val;
+    struct s_node *brother;
+    struct s_node *children;
+
 } t_node ;
 
 typedef struct	s_data
@@ -44,12 +45,20 @@ typedef struct	s_data
 	int			end;
 }				t_data;
 
+typedef struct	s_links
+{	
+	char			*a;
+	char			*b;
+	struct s_links	*next;
+}				t_links;
+
 typedef struct s_file t_file;
 
 struct s_file
 {
 	char		*line;
 	t_file		*next;
+
 };
 
 void	add_end(t_file **file);
